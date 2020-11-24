2f10654a9435c996ffedeca69eed37d59f0420b8
Son Mai <14060682+sonhmai@users.noreply.github.com>
2020-11-24 15:28:59 +0700

    Update and rename kubectl.md to kubectl.sh

--- kubectl.sh
+++ kubectl.sh
@@ -2,9 +2,13 @@
 
 ## act on multiple deployments,...
 
-### update deployment by rollout
+# update deployment by rollout
 kubectl get deployment | grep $DEPLOYMENT | awk '{print $1}' | xargs -n 1 kubectl rollout restart deployment 
 
-### update deployment by scaling
+# update deployment by scaling
 kubectl get deployment | grep $DEPLOYMENT | awk '{print $1}' | xargs -n 1 kubectl scale deployment --replicas 0
 kubectl get deployment | grep $DEPLOYMENT | awk '{print $1}' | xargs -n 1 kubectl scale deployment --replicas 1
+
+# exec
+kubectl exec -it $PODNAME -- /bin/bash
+kubectl exec $PODNAME -- env. # get env of a pod
